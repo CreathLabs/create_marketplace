2e33fa84798b5603bfc17b51400dc494db5bacde
Abdulmalik <mackquadrizz@gmail.com>
2024-11-01 12:54:50 +0100

    uncomment the code

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -12,6 +12,8 @@ import {
   getTopArtists,
   getTopNfts,
   getTopExhibitions,
+  getTopCollectibless,
+  getTopBlogs
 } from "@/actions";
 
 const artists = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -20,8 +22,8 @@ export default async function Home() {
   const topNfts = await getTopNfts();
   const artists = await getTopArtists();
   const exhibitions = await getTopExhibitions();
-  // const collectibles = await getTopCollectibless();
-  // const blogs = await getTopBlogs();
+  const collectibles = await getTopCollectibless();
+  const blogs = await getTopBlogs();
 
 
 
@@ -70,11 +72,11 @@ export default async function Home() {
               className="hidden lg:flex"
             />
           </div>
-          {/* <div className="grid lg:grid-cols-3 gap-16">
+          <div className="grid lg:grid-cols-3 gap-16">
             {collectibles.map((item) => (
               <Collectible key={item.id} {...item} />
             ))}
-          </div> */}
+          </div>
           <div className="w-full flex justify-center lg:hidden">
             <Button text="View More" textStyles="w-[144px]" />
           </div>
@@ -155,11 +157,11 @@ export default async function Home() {
               className="hidden lg:flex"
             />
           </div>
-          {/* <div className="grid lg:grid-cols-3 gap-[34px]">
+          <div className="grid lg:grid-cols-3 gap-[34px]">
             {blogs?.map((blog) => (
               <BlogCard key={blog.id} {...blog} />
             ))}
-          </div> */}
+          </div>
         </div>
       </div>
     </main>
