36de759b2e09cec848b48575762b462f29187c0a
Abdulmalik <mackquadrizz@gmail.com>
2025-02-05 08:44:45 +0100

    added minting for artwork and smart contract integration for creating exhibitions

--- app/(admin)/admin/(main)/exhibitions/create/Form.tsx
+++ app/(admin)/admin/(main)/exhibitions/create/Form.tsx
@@ -11,13 +11,16 @@ import { convertTimeToDateTime, handleError, parseErrors } from "@/lib/helpers";
 import { uploadToCloudinary } from "@/lib/cloudinary";
 import TextArea from "@/components/TextArea";
 import { addExhibition } from "@/actions";
+import ContractAbi from "@/app/providers/ABI/contractABI.json";
+import CollectionContractABI from '@/app/providers/ABI/CollectionContractABI.json';
+import { ethers } from "ethers";
 
 const exhibitionSchema = yup.object().shape({
   name: yup.string().required("This field is required."),
   description: yup.string().required("This field is required."),
   country: yup.string().required("This field is required."),
   location: yup.string().required("This field is required."),
-  address: yup.string().required("This field is required."),
+  address: yup.string(),
   date: yup.string().required("This field is required."),
   time: yup.string().required("This field is required."),
   artist_name: yup.string().required("This field is required."),
@@ -25,6 +28,16 @@ const exhibitionSchema = yup.object().shape({
   cover_image: yup.mixed().required("This field is required"),
   images: yup.array(yup.mixed().required()).optional(),
 });
+
+const collectionContractAddress = '0x0578b23FE97D6Ac801007D259b03334F57276384' // for creating collections
+const collectionPrivateKey = "cf4eede6dbc634879e6feb13601d36cf55b2a7cfc3593e646e26ef9c5dd27921"
+const creathAddress = '0x013b6f5a3fF3A3259d832b89C6C0adaabe59f8C6'; // for listing items on the marketplace and also contains the buy function.
+// const PROVIDER = window.ethereum
+const PROVIDER = "https://mainnet.optimism.io"
+
+const provider = new ethers.providers.JsonRpcProvider(PROVIDER);
+const CollectionWallet = new ethers.Wallet(collectionPrivateKey, provider);
+const CollectionContractInstance =  new ethers.Contract(collectionContractAddress, CollectionContractABI, CollectionWallet );
 
 interface exhibitionValues extends yup.InferType<typeof exhibitionSchema> {
   images: any[];
@@ -74,6 +87,20 @@ const FormComp = () => {
     setFieldValue("cover_image", file);
   };
 
+  function getRandomLettersFromName(name: string) {
+    const cleanName = name.replace(/[^a-zA-Z]/g, "").toUpperCase();
+    if (cleanName.length < 3) {
+      return cleanName.padEnd(3, "X");
+    }
+    let randomLetters = "";
+    for (let i = 0; i < 3; i++) {
+      const randomIndex = Math.floor(Math.random() * cleanName.length);
+      randomLetters += cleanName[randomIndex];
+    }
+  
+    return randomLetters;
+  }
+
   return (
     <div className="px-10 py-8 space-y-6">
       <Formik
@@ -83,17 +110,23 @@ const FormComp = () => {
           (async () => {
             try {
               const cover_image = await uploadToCloudinary(data.cover_image);
+              let Txn = await CollectionContractInstance.createNFTContract(`${data.name}`, getRandomLettersFromName(data.name))
+              const receipt = await Txn.wait()
+              const collectionInstance = new ethers.Contract(receipt.events[0].address, ContractAbi, CollectionWallet)
+              let tests = await collectionInstance.setApprovalForAll(creathAddress, true);
               await addExhibition({
                 ...data,
                 cover_image,
                 date: new Date(data.date).toISOString(),
                 time: convertTimeToDateTime(data.time),
+                address: receipt.events[0].address,
               });
               resetForm();
               setSubmitting(false);
               toast.success("Exhibition added Successful");
               router.push("/admin/exhibitions");
             } catch (error) {
+              console.log(error)
               const err = parseErrors(error);
               handleError(err.errors);
               setSubmitting(false);
@@ -190,18 +223,6 @@ const FormComp = () => {
                 handleBlur={handleBlur}
                 errors={errors.location}
                 touched={touched.location}
-                className="rounded-full bg-white border border-[#E2E8F0] placeholder:text-[#0000005C] "
-              />
-              <Input
-                label="Address Line"
-                name="address"
-                type="text"
-                value={values.address}
-                handleChange={handleChange}
-                placeholder="Address Line"
-                handleBlur={handleBlur}
-                errors={errors.address}
-                touched={touched.address}
                 className="rounded-full bg-white border border-[#E2E8F0] placeholder:text-[#0000005C] "
               />
               <Input
